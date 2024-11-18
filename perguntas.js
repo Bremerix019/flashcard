2b8862aa325d13b40581d899c976db02a4ef8ca9
Bremerix019 <arthurbremer9@gmail.com>
2024-11-18 11:31:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Esportes',
+    'Qual é o esporte mais popular do mundo?',
+    'O esporte mais popular do mundo é o futebol.'
+)
+
+criaCartao(
+    'Esportes',
+    'Quantos jogadores há em um time de vôlei?',
+    'Um time de vôlei é composto por 6 jogadores em quadra.'
+)
+
+criaCartao(
+    'Esportes',
+    'Qual é a origem das Olimpíadas?',
+    'As Olimpíadas têm origem na Grécia Antiga, onde eram realizadas em honra aos deuses.'
+)
+
+criaCartao(
+    'Esportes',
+    'O que é um touchdown no futebol americano?',
+    'Um touchdown é quando um jogador cruza a linha de gol adversária com a bola, marcando 6 pontos.'
+)
